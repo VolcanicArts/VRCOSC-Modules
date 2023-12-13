aba9bcba538cef4d485ce22846bcc0b66570e1da
VolcanicArts <VolcanicArtsBP@Gmail.com>
2023-12-13 23:18:52 +0000

    Give ClockModule more parameters and remove timezone changing

--- VRCOSC.Modules/Clock/ClockModule.cs
+++ VRCOSC.Modules/Clock/ClockModule.cs
@@ -14,70 +14,76 @@ namespace VRCOSC.Modules.Clock;
 [ModulePrefab("VRCOSC-Watch", "https://github.com/VolcanicArts/VRCOSC/releases/download/latest/VRCOSC-Watch.unitypackage")]
 public sealed class ClockModule : AvatarModule
 {
-    private DateTime time;
-
     protected override void OnLoad()
     {
         CreateToggle(ClockSetting.SmoothSecond, "Smooth Second", "If the seconds value should be smoothed", false);
         CreateToggle(ClockSetting.SmoothMinute, "Smooth Minute", "If the minutes value should be smoothed", true);
         CreateToggle(ClockSetting.SmoothHour, "Smooth Hour", "If the hours value should be smoothed", true);
+        CreateToggle(ClockSetting.Mode, "12/24 Mode", "Off for 12 hour. On for 24 hour", false);
         CreateDropdown(ClockSetting.Timezone, "Timezone", "The timezone the clock should follow", ClockTimeZone.Local);
 
-        RegisterParameter<float>(ClockParameter.Hours, "VRCOSC/Clock/Hours", ParameterMode.Write, "Hours", "The current hour normalised between 0 and 1");
-        RegisterParameter<float>(ClockParameter.Minutes, "VRCOSC/Clock/Minutes", ParameterMode.Write, "Minutes", "The current minute normalised between 0 and 1");
-        RegisterParameter<float>(ClockParameter.Seconds, "VRCOSC/Clock/Seconds", ParameterMode.Write, "Seconds", "The current second normalised between 0 and 1");
         RegisterParameter<bool>(ClockParameter.Period, "VRCOSC/Clock/Period", ParameterMode.Write, "Period", "False for AM. True for PM");
+        RegisterParameter<bool>(ClockParameter.Mode, "VRCOSC/Clock/Mode", ParameterMode.Write, "Mode", "False for 12 hour. True for 24 hour");
+        RegisterParameter<int>(ClockParameter.HoursI, "VRCOSC/Clock/HoursI", ParameterMode.Write, "Hours Int", "The current hour (0-11/23)");
+        RegisterParameter<int>(ClockParameter.MinutesI, "VRCOSC/Clock/MinutesI", ParameterMode.Write, "Minutes Int", "The current minute (0-11/23)");
+        RegisterParameter<int>(ClockParameter.SecondsI, "VRCOSC/Clock/SecondsI", ParameterMode.Write, "Seconds Int", "The current second (0-11/23)");
+        RegisterParameter<float>(ClockParameter.HoursF, "VRCOSC/Clock/HoursF", ParameterMode.Write, "Hours Float", "The current hour normalised between 0 and 1");
+        RegisterParameter<float>(ClockParameter.MinutesF, "VRCOSC/Clock/MinutesF", ParameterMode.Write, "Minutes Float", "The current minute normalised between 0 and 1");
+        RegisterParameter<float>(ClockParameter.SecondsF, "VRCOSC/Clock/SecondsF", ParameterMode.Write, "Seconds Float", "The current second normalised between 0 and 1");
 
+        CreateGroup("Tweaks", ClockSetting.Mode, ClockSetting.Timezone);
         CreateGroup("Smoothing", ClockSetting.SmoothHour, ClockSetting.SmoothMinute, ClockSetting.SmoothSecond);
-        CreateGroup("Timezone", ClockSetting.Timezone);
     }
 
     [ModuleUpdate(ModuleUpdateMode.Custom)]
     private void updateVariables()
     {
-        time = timezoneToTime(GetSettingValue<ClockTimeZone>(ClockSetting.Timezone));
+        var time = DateTime.Now;
+
+        var hoursInt = GetSettingValue<bool>(ClockSetting.Mode) ? time.Hour : time.Hour % 12;
+        var minutesInt = GetSettingValue<bool>(ClockSetting.Mode) ? time.Minute : time.Minute % 60;
+        var secondsInt = GetSettingValue<bool>(ClockSetting.Mode) ? time.Second : time.Second % 60;
 
         var hours = GetSettingValue<bool>(ClockSetting.SmoothHour) ? getSmoothedHours(time) : time.Hour;
         var minutes = GetSettingValue<bool>(ClockSetting.SmoothMinute) ? getSmoothedMinutes(time) : time.Minute;
         var seconds = GetSettingValue<bool>(ClockSetting.SmoothSecond) ? getSmoothedSeconds(time) : time.Second;
 
-        var hourNormalised = hours % 12f / 12f;
+        var hourNormalised = GetSettingValue<bool>(ClockSetting.Mode) ? hours / 24f : hours % 12f / 12f;
         var minuteNormalised = minutes / 60f;
         var secondNormalised = seconds / 60f;
 
-        SendParameter(ClockParameter.Hours, hourNormalised);
-        SendParameter(ClockParameter.Minutes, minuteNormalised);
-        SendParameter(ClockParameter.Seconds, secondNormalised);
         SendParameter(ClockParameter.Period, string.Equals(time.ToString("tt", CultureInfo.InvariantCulture), "PM", StringComparison.InvariantCultureIgnoreCase));
+        SendParameter(ClockParameter.Mode, GetSettingValue<bool>(ClockSetting.Mode));
+
+        SendParameter(ClockParameter.HoursI, hoursInt);
+        SendParameter(ClockParameter.MinutesI, minutesInt);
+        SendParameter(ClockParameter.SecondsI, secondsInt);
+
+        SendParameter(ClockParameter.HoursF, hourNormalised);
+        SendParameter(ClockParameter.MinutesF, minuteNormalised);
+        SendParameter(ClockParameter.SecondsF, secondNormalised);
     }
 
     private static float getSmoothedSeconds(DateTime time) => time.Second + time.Millisecond / 1000f;
     private static float getSmoothedMinutes(DateTime time) => time.Minute + getSmoothedSeconds(time) / 60f;
     private static float getSmoothedHours(DateTime time) => time.Hour + getSmoothedMinutes(time) / 60f;
 
-    private static DateTime timezoneToTime(ClockTimeZone timeZone) => timeZone switch
-    {
-        ClockTimeZone.Local => DateTime.Now,
-        ClockTimeZone.UTC => DateTime.UtcNow,
-        ClockTimeZone.GMT => TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time")),
-        ClockTimeZone.EST => TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time")),
-        ClockTimeZone.CST => TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById("Central Standard Time")),
-        ClockTimeZone.MNT => TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById("Mountain Standard Time")),
-        ClockTimeZone.PST => TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time")),
-        _ => throw new ArgumentOutOfRangeException(nameof(timeZone), timeZone, null)
-    };
-
     private enum ClockParameter
     {
-        Hours,
-        Minutes,
-        Seconds,
-        Period
+        Period,
+        Mode,
+        HoursI,
+        MinutesI,
+        SecondsI,
+        HoursF,
+        MinutesF,
+        SecondsF
     }
 
     private enum ClockSetting
     {
         Timezone,
+        Mode,
         SmoothSecond,
         SmoothMinute,
         SmoothHour
