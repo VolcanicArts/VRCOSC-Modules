22bf1d80eac6a4bd739849aad472ab862ad8a8e7
VolcanicArts <VolcanicArtsBP@Gmail.com>
2023-12-22 16:32:45 +0000

    Port Hyperate module

---
+++ VRCOSC.Modules/Hyperate/HyperateModule.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
+// See the LICENSE file in the repository root for full license text.
+
+using VRCOSC.SDK;
+using VRCOSC.SDK.Modules.Heartrate;
+
+namespace VRCOSC.Modules.Hyperate;
+
+[ModuleTitle("HypeRate")]
+[ModuleDescription("Connects to HypeRate.io and sends your heartrate to VRChat")]
+public sealed class HypeRateModule : HeartrateModule<HypeRateProvider>
+{
+    protected override HypeRateProvider CreateProvider() => new(GetSettingValue<string>(HypeRateSetting.Id)!, OfficialModuleSecrets.GetSecret(OfficialModuleSecretsKeys.Hyperate));
+
+    protected override void OnLoad()
+    {
+        CreateTextBox(HypeRateSetting.Id, "HypeRate ID", "Your HypeRate ID given on your device", string.Empty);
+
+        CreateGroup("Access", HypeRateSetting.Id);
+
+        base.OnLoad();
+    }
+
+    protected override Task<bool> OnModuleStart()
+    {
+        if (string.IsNullOrEmpty(GetSettingValue<string>(HypeRateSetting.Id)))
+        {
+            Log("Cannot connect to HypeRate. Please enter an Id");
+            return Task.FromResult(false);
+        }
+
+        return base.OnModuleStart();
+    }
+
+    [ModuleUpdate(ModuleUpdateMode.Custom, true, 10000)]
+    private void sendWsHeartbeat()
+    {
+        HeartrateProvider?.SendWsHeartBeat();
+    }
+
+    private enum HypeRateSetting
+    {
+        Id
+    }
+}
---
+++ VRCOSC.Modules/Hyperate/Models.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
+// See the LICENSE file in the repository root for full license text.
+
+using Newtonsoft.Json;
+
+namespace VRCOSC.Modules.Hyperate;
+
+public sealed class EventModel
+{
+    [JsonProperty("event")]
+    public string Event = null!;
+}
+
+public sealed class HeartBeatModel
+{
+    [JsonProperty("event")]
+    public string Event = "heartbeat";
+
+    [JsonProperty("payload")]
+    public WebSocketHeartBeatPayload Payload = new();
+
+    [JsonProperty("ref")]
+    public int Ref;
+
+    [JsonProperty("topic")]
+    public string Topic = "phoenix";
+}
+
+public sealed class WebSocketHeartBeatPayload
+{
+}
+
+public sealed class HeartRateUpdateModel
+{
+    [JsonProperty("payload")]
+    public HeartRateUpdatePayload Payload = null!;
+}
+
+public class HeartRateUpdatePayload
+{
+    [JsonProperty("hr")]
+    public int HeartRate;
+}
+
+public sealed class JoinChannelModel
+{
+    [JsonProperty("event")]
+    public string Event = "phx_join";
+
+    [JsonProperty("payload")]
+    public JoinChannelPayload Payload = new();
+
+    [JsonProperty("ref")]
+    public int Ref;
+
+    [JsonProperty("topic")]
+    public string Topic = null!;
+
+    [JsonIgnore]
+    public string Id
+    {
+        set => Topic = "hr:" + value;
+    }
+}
+
+public sealed class JoinChannelPayload
+{
+}
+
+public sealed class PhxReplyModel
+{
+    [JsonProperty("payload")]
+    public PhxReplyPayload Payload = null!;
+}
+
+public sealed class PhxReplyPayload
+{
+    [JsonProperty("status")]
+    public string Status = null!;
+}
