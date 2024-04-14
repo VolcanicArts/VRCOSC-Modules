f2c0954606288364297705558753d29d8baf86e3
VolcanicArts <VolcanicArtsBP@Gmail.com>
2024-04-15 00:50:19 +0100

    Manually define assembly info

---
+++ VRCOSC.Modules/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
+// See the LICENSE file in the repository root for full license text.
+
+using System.Reflection;
+
+[assembly: AssemblyTitle("Official Modules")]
+[assembly: AssemblyDescription("VRCOSC's official modules")]
+[assembly: AssemblyVersion("2024.415.0.0")]
+[assembly: AssemblyFileVersion("2024.415.0.0")]
