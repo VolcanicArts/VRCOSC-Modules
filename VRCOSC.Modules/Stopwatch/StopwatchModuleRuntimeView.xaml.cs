9048237f255c514fc9138b007529b3477b30bab0
VolcanicArts <VolcanicArtsBP@Gmail.com>
2024-08-05 19:13:37 +0100

    Add missing headers

--- VRCOSC.Modules/Stopwatch/StopwatchModuleRuntimeView.xaml.cs
+++ VRCOSC.Modules/Stopwatch/StopwatchModuleRuntimeView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
+// See the LICENSE file in the repository root for full license text.
+
+using System.Windows;
 
 namespace VRCOSC.Modules.Stopwatch;
 
