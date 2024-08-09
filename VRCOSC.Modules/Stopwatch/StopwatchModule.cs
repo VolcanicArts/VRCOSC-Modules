80369051f83eb26bffa6af6d896f22c9dc5a37f1
VolcanicArts <VolcanicArtsBP@Gmail.com>
2024-08-09 20:09:07 +0100

    Add parameters and more functionality to StopwatchModule

--- VRCOSC.Modules/Stopwatch/StopwatchModule.cs
+++ VRCOSC.Modules/Stopwatch/StopwatchModule.cs
@@ -11,16 +11,27 @@ namespace VRCOSC.Modules.Stopwatch;
 [ModuleType(ModuleType.Generic)]
 public class StopwatchModule : Module
 {
-    private StopwatchModuleRuntimeView runtimeView;
-
+    private StopwatchModuleRuntimeView runtimeView = null!;
     private TimeSpan currentTime;
-    private bool shouldAddTime;
+    private StopwatchState currentState;
 
     protected override void OnPreLoad()
     {
+        CreateToggle(StopwatchSetting.SmoothSecond, "Smooth Second", "If the Second Normalised parameter should be smoothed", true);
+        CreateToggle(StopwatchSetting.SmoothMinute, "Smooth Minute", "If the Minute Normalised parameter should be smoothed", true);
+
         RegisterParameter<bool>(StopwatchParameter.Start, "VRCOSC/Stopwatch/Start", ParameterMode.Read, "Start", "Starts the stopwatch when this parameter becomes true");
         RegisterParameter<bool>(StopwatchParameter.Pause, "VRCOSC/Stopwatch/Pause", ParameterMode.Read, "Pause", "Pauses the stopwatch when this parameter becomes true");
         RegisterParameter<bool>(StopwatchParameter.Stop, "VRCOSC/Stopwatch/Stop", ParameterMode.Read, "Stop", "Stops and resets the stopwatch when this parameter becomes true");
+
+        RegisterParameter<int>(StopwatchParameter.State, "VRCOSC/Stopwatch/State", ParameterMode.Write, "State", "The state of the stopwatch\n0 - Stopped\n1 - Paused\n2 - Started");
+
+        RegisterParameter<int>(StopwatchParameter.MinuteValue, "VRCOSC/Stopwatch/Minute/Value", ParameterMode.Write, "Minute Value", "The minute value of the stopwatch (0-59)");
+        RegisterParameter<float>(StopwatchParameter.MinuteNormalised, "VRCOSC/Stopwatch/Minute/Normalised", ParameterMode.Write, "Minute Normalised",
+            "The minute value of the stopwatch normalised between 0 and 1");
+        RegisterParameter<int>(StopwatchParameter.SecondValue, "VRCOSC/Stopwatch/Second/Value", ParameterMode.Write, "Second Value", "The second value of the stopwatch (0-59)");
+        RegisterParameter<float>(StopwatchParameter.SecondNormalised, "VRCOSC/Stopwatch/Second/Normalised", ParameterMode.Write, "Second Normalised",
+            "The second value of the stopwatch normalised between 0 and 1");
     }
 
     protected override void OnPostLoad()
@@ -35,22 +46,50 @@ public class StopwatchModule : Module
     protected override Task<bool> OnModuleStart()
     {
         currentTime = TimeSpan.Zero;
-        shouldAddTime = false;
-
-        ChangeState(StopwatchState.Stopped);
+        changeState(StopwatchState.Stopped);
 
         SetRuntimeView(runtimeView = new StopwatchModuleRuntimeView(this));
 
         return Task.FromResult(true);
     }
 
+    private void changeState(StopwatchState state)
+    {
+        ChangeState(state);
+        SendParameter(StopwatchParameter.State, (int)state);
+        currentState = state;
+
+        if (state == StopwatchState.Stopped)
+        {
+            currentTime = TimeSpan.Zero;
+
+            SendParameter(StopwatchParameter.MinuteValue, 0);
+            SendParameter(StopwatchParameter.SecondValue, 0);
+
+            SendParameter(StopwatchParameter.MinuteNormalised, 0f);
+            SendParameter(StopwatchParameter.SecondNormalised, 0f);
+        }
+    }
+
     [ModuleUpdate(ModuleUpdateMode.Custom)]
     private void updateCurrentTime()
     {
-        if (!shouldAddTime) return;
+        if (currentState != StopwatchState.Started) return;
 
         currentTime += TimeSpan.FromMilliseconds(50);
+
+        var minutesValue = GetSettingValue<bool>(StopwatchSetting.SmoothMinute) ? getSmoothedMinute(currentTime) : currentTime.Minutes;
+        var secondsValue = GetSettingValue<bool>(StopwatchSetting.SmoothSecond) ? getSmoothedSecond(currentTime) : currentTime.Seconds;
+
+        SendParameter(StopwatchParameter.MinuteValue, currentTime.Minutes);
+        SendParameter(StopwatchParameter.SecondValue, currentTime.Seconds);
+
+        SendParameter(StopwatchParameter.MinuteNormalised, minutesValue / 60f);
+        SendParameter(StopwatchParameter.SecondNormalised, secondsValue / 60f);
     }
+
+    private static float getSmoothedSecond(TimeSpan time) => time.Seconds + time.Milliseconds / 1000f;
+    private static float getSmoothedMinute(TimeSpan time) => time.Minutes + getSmoothedSecond(time) / 60f;
 
     [ModuleUpdate(ModuleUpdateMode.ChatBox)]
     private void updateVariables()
@@ -63,54 +102,57 @@ public class StopwatchModule : Module
         switch (parameter.Lookup)
         {
             case StopwatchParameter.Start when parameter.GetValue<bool>():
-                shouldAddTime = true;
-                ChangeState(StopwatchState.Started);
+                changeState(StopwatchState.Started);
                 break;
 
             case StopwatchParameter.Pause when parameter.GetValue<bool>():
-                shouldAddTime = false;
-                ChangeState(StopwatchState.Paused);
+                changeState(StopwatchState.Paused);
                 break;
 
             case StopwatchParameter.Stop when parameter.GetValue<bool>():
-                shouldAddTime = false;
-                currentTime = TimeSpan.Zero;
-                ChangeState(StopwatchState.Stopped);
+                changeState(StopwatchState.Stopped);
                 break;
         }
     }
 
     public void StartStopwatch()
     {
-        shouldAddTime = true;
-        ChangeState(StopwatchState.Started);
+        changeState(StopwatchState.Started);
     }
 
     public void PauseStopwatch()
     {
-        shouldAddTime = false;
-        ChangeState(StopwatchState.Paused);
+        changeState(StopwatchState.Paused);
     }
 
     public void StopStopwatch()
     {
-        shouldAddTime = false;
-        currentTime = TimeSpan.Zero;
-        ChangeState(StopwatchState.Stopped);
+        changeState(StopwatchState.Stopped);
+    }
+
+    private enum StopwatchSetting
+    {
+        SmoothSecond,
+        SmoothMinute
     }
 
     private enum StopwatchParameter
     {
         Start,
         Pause,
-        Stop
+        Stop,
+        State,
+        MinuteValue,
+        SecondValue,
+        MinuteNormalised,
+        SecondNormalised
     }
 
     private enum StopwatchState
     {
-        Started,
-        Paused,
-        Stopped,
+        Stopped = 0,
+        Paused = 1,
+        Started = 2
     }
 
     private enum StopwatchVariable
--- VRCOSC.Modules/Stopwatch/StopwatchModuleRuntimeView.xaml.cs
+++ VRCOSC.Modules/Stopwatch/StopwatchModuleRuntimeView.xaml.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
+// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
 // See the LICENSE file in the repository root for full license text.
 
 using System.Windows;
@@ -21,12 +21,12 @@ public partial class StopwatchModuleRuntimeView
         module.StartStopwatch();
     }
 
-    private void Stop_OnClick(object sender, RoutedEventArgs e)
+    private void Pause_OnClick(object sender, RoutedEventArgs e)
     {
         module.PauseStopwatch();
     }
 
-    private void Reset_OnClick(object sender, RoutedEventArgs e)
+    private void Stop_OnClick(object sender, RoutedEventArgs e)
     {
         module.StopStopwatch();
     }
