93c385365103661529f7fa9004743fe212d42634
VolcanicArts <VolcanicArtsBP@Gmail.com>
2024-12-01 15:53:24 +0000

    Update for merge rename

--- VRCOSC.Modules/OfficialModuleSecrets.cs
+++ VRCOSC.Modules/OfficialModuleSecrets.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
+// Copyright (c) VolcanicArts. Licensed under the GPL-3.0 License.
 // See the LICENSE file in the repository root for full license text.
 
 namespace VRCOSC.Modules;
@@ -15,6 +15,6 @@ public static class OfficialModuleSecrets
 public enum OfficialModuleSecretsKeys
 {
     Hyperate,
-    Weather,
-    ExchangeRate
+    Weather
+
 }
